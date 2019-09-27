bf1dd16a528528628a2e88be08a6f3d91c9cd663
Your Name <snakexv>
2019-09-27 13:04:04 +0200

    Nombre Magique v1

--- NombreMagique/Nombre_magique.cs
+++ NombreMagique/Nombre_magique.cs
@@ -7,15 +7,18 @@ namespace NombreMagique
         static void Main(string[] args)
         {
             int  nb_mag = 5;
-            
-            Console.WriteLine("Please choose a number between 0 and 10!");
+            int NumberOfLife = 4;
+            while (NumberOfLife > 0)
+             {
+
+            Console.WriteLine("Can you guess the magic number (there is still " + NumberOfLife + " lives left) : " );
             String nombre = Console.ReadLine();
 
             int nombre_num = 0;
             if (int.TryParse(nombre, out nombre_num) == true)
             {
-                //while (nombre_num != nb_mag)
-                //{
+                
+               
                     if (nombre_num > nb_mag)
                     {
                         Console.WriteLine("The number is higher, please try again");
@@ -27,15 +30,26 @@ namespace NombreMagique
                     else
                     {
                         Console.WriteLine("Congrat ! You found the good one !");
+                        break;
                     };
-                //};
+
+                    NumberOfLife--;
             }
             else
             {
                 Console.WriteLine("Please enter a number !");
             };
-
-
+                
+            };
+            if (NumberOfLife>0)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("Sorry Game over !");
+            }
+            
         }
     }
 }
