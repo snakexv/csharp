fae8f65aab16418866aefeec2f5f4047312dfde6
Your Name <snakexv>
2019-09-30 17:00:44 +0200

    version3

--- NombreMagique/Nombre_magique.cs
+++ NombreMagique/Nombre_magique.cs
@@ -6,19 +6,30 @@ namespace NombreMagique
     {
         static void Main(string[] args)
         {
-            int  nb_mag = 5;
-            int NumberOfLife = 4;
+            const int min = 1;
+            const int max = 10;
+            const int NumberLife = 4;
+            Random random = new Random();
+            int nb_mag = random.Next(min, max);
+            int NumberOfLife = NumberLife;
+
             while (NumberOfLife > 0)
              {
 
-            Console.WriteLine("Can you guess the magic number (there is still " + NumberOfLife + " lives left) : " );
+            Console.WriteLine("Can you guess the magic number between "+ min +" and "  +max +" (there is still " + NumberOfLife + " lives left) : " );
             String nombre = Console.ReadLine();
+            
 
             int nombre_num = 0;
             if (int.TryParse(nombre, out nombre_num) == true)
             {
                 
-               
+                if((nombre_num > max) | (nombre_num < min))
+                        {
+                        Console.WriteLine("You didn't guess a number between " + min + " and " + max +", please try again");
+                        NumberOfLife++;
+                        }
+                else
                     if (nombre_num > nb_mag)
                     {
                         Console.WriteLine("The number is higher, please try again");
@@ -47,7 +58,7 @@ namespace NombreMagique
             }
             else
             {
-                Console.WriteLine("Sorry Game over !");
+                Console.WriteLine("Sorry Game over ! The number that you had to guess was : " + nb_mag);
             }
             
         }
