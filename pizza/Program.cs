81649cd9e7a1de2dea1091e2ade4183ab6f33373
Your Name <snakexv>
2019-10-07 17:03:36 +0200

    pizza project

---
+++ pizza/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace pizza
+{
+    class Program
+    {
+        public static void PrintList(List<string> liste)
+        {
+            foreach (string p in liste)
+            {
+                Console.WriteLine(p);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            const int Nb_pizza = 10;
+            string[] pizza = new string[Nb_pizza];
+            
+            for (int i = 0; i < Nb_pizza;i++)
+                {
+                    int j = i + 1;
+                    pizza[i] = "Pizza "+ j;
+                }
+
+            List<string> Pizza = pizza.ToList();
+
+            while (true)
+            {
+
+            Console.Write("Please add a new pizza (or nothing to close) : ");
+                string NewPizza = Console.ReadLine();
+
+                
+
+            if (NewPizza=="")
+                {
+
+                    Console.WriteLine("");
+
+                    break;
+                  
+                }
+            else
+                {
+                    Pizza.Add(NewPizza);
+                }
+
+            }
+            PrintList(Pizza);
+
+        }
+    }
+}
