602f46dd0ca62793ae3439b88cf9f2be152b9b91
Your Name <snakexv>
2019-10-10 18:08:36 +0200

    liste of pizzas

---
+++ pizzaliste/ListePizza/ListePizza/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListePizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<pizza> pizzas = new List<pizza>();
+            pizzas.Add(new pizza("vegetarienne", 12));
+            pizzas.Add(new pizza("oriental", 10));
+            pizzas.Add(new pizza("4 fromages", 14));
+            pizzas.Add(new pizza("thon", 11));
+            pizzas.Add(new pizza("4 saisons", 15));
+            
+            foreach (pizza p in pizzas)
+            {
+                p.PrintList();
+            }
+        }
+    }
+}
---
+++ pizzaliste/ListePizza/ListePizza/pizza.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListePizza
+{
+    public class pizza
+    {
+        List<string> listpizza = new List<string>(5);
+        //List<string> prixPizza = new List<string>();
+        string nom;
+        int prix;
+
+        public pizza(string nom, int prix)
+        {
+            this.nom = nom;
+            this.prix = prix;
+        }
+
+
+
+        public void PrintList()
+        {
+            Console.WriteLine("Pizza : " + nom + " - " + prix + " Euros ");
+        }
+
+
+    }
+}
