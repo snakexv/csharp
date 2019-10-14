31b1bc06c7f19dde0e8231a43032e2f90e5fdc5f
Your Name <snakexv>
2019-10-14 17:08:16 +0200

    update1

--- pizzaliste/Program.cs
+++ pizzaliste/Program.cs
@@ -5,7 +5,7 @@ namespace ListePizza
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             List<pizza> pizzas = new List<pizza>();
             pizzas.Add(new pizza("vegetarienne", 12));
@@ -13,11 +13,21 @@ namespace ListePizza
             pizzas.Add(new pizza("4 fromages", 14));
             pizzas.Add(new pizza("thon", 11));
             pizzas.Add(new pizza("4 saisons", 15));
-            
+
+            pizzas.Sort((pizza x, pizza y) =>
+            {
+                return x.Prix.CompareTo(y.Prix);
+            });
+
+
             foreach (pizza p in pizzas)
             {
                 p.PrintList();
             }
+
+            pizza.printCheapAndExpensive();
+
+
         }
     }
 }
--- pizzaliste/pizza.cs
+++ pizzaliste/pizza.cs
@@ -5,17 +5,30 @@ namespace ListePizza
 {
     public class pizza
     {
+        public static pizza pizzaLaMoinsChere;
+        public static pizza pizzaLaPlusChere;
         List<string> listpizza = new List<string>(5);
         //List<string> prixPizza = new List<string>();
-        string nom;
+        string nom; 
         int prix;
 
         public pizza(string nom, int prix)
         {
             this.nom = nom;
             this.prix = prix;
+            if (pizzaLaMoinsChere == null){ pizzaLaMoinsChere = this; }
+            else 
+                { if (prix > pizzaLaPlusChere.prix) {pizzaLaPlusChere = this; }}
+            if (pizzaLaPlusChere == null) { pizzaLaPlusChere = this; }
+            else
+            { if (prix < pizzaLaMoinsChere.prix) { pizzaLaMoinsChere = this; } }
+
         }
 
+       public int Prix
+        {
+            get { return prix; }
+        }
 
 
         public void PrintList()
@@ -23,6 +36,16 @@ namespace ListePizza
             Console.WriteLine("Pizza : " + nom + " - " + prix + " Euros ");
         }
 
+        public static void printCheapAndExpensive()
+        {
+            if (pizzaLaPlusChere != null) { Console.WriteLine("Pizza la plus chère "); pizzaLaPlusChere.PrintList(); }
+            ;
+            if (pizzaLaMoinsChere != null) { Console.WriteLine("Pizza la moins chère "); pizzaLaMoinsChere.PrintList(); }
+        }
 
+        //internal void PrixCompareTo(object prix)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
